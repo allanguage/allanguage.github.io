326ac74c71cf269e20a179f3565ea08613b5113c
allanguage <77337044+allanguage@users.noreply.github.com>
2021-01-12 17:55:32 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(function () {
+
+  $(window).on('load', function () {
+    $('.preloader').delay(50).fadeOut('slow', function () {
+      $(this).attr('style', 'display: none !important');
+    });
+    wow = new WOW();
+    wow.init();
+  });
+
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() >= 200) {
+      $('.navbar').addClass('fixed-top');
+    } else if ($(this).scrollTop() == 0) {
+      $('.navbar').removeClass('fixed-top');
+    }
+  });
+
+  var text = "ALlanguage";
+
+  for (var i in text) {
+    if (text[i] === " ") {
+      $(".wavetext").append($("<span>").html("&nbsp;"));
+    } else {
+      $(".wavetext").append($("<span>").text(text[i]));
+    }
+  }
+
+});
